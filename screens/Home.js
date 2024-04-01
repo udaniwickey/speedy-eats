7287976ec46a185a0e34cdfe36777cdeb1e52182
udaniwickey <udaniwickey@gmail.com>
2024-04-01 22:49:06 +0530

    ResturantItem.js half-completed.

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,27 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
+
+const YELP_API_KEY = ""
+
 
 export default function Home() {
+  
+  const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
+
   return (
     <SafeAreaView style ={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
         <SearchBar />
       </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
     </SafeAreaView>
   )
 }
